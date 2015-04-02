b6eec11deeb6b5082bd984f93e09e4218b016393
edoecohen <edoecohen@gmail.com>
2015-04-01 18:48:14 -0700

    Implement findNRooksSolution with recursion

--- src/solvers.js
+++ src/solvers.js
@@ -14,13 +14,48 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var board = new Board({n: n});
+
+  // build a for loop that iterates over each colum
+
+  // for each square in that first row --> kick off our decision tree
+  // in the decision tree, recurse over each child
+  var solution;
+  var traverse = function(row, col){
+
+    board.togglePiece(row, col);
+
+    // if no conflict, traverse children, unless we're in last row
+    if(!board.hasAnyRooksConflicts()){
+      for(var i = 0; i < n; i++){
+
+        // no conflict & no more children --> we have our solution
+        if (row+1 === n){
+          solution = board;
+        }
+
+        // no conflict & yes children --> traverse all children
+        else {
+          traverse(row+1, i)
+        }
+      }
+    }
+
+    // else, there is a conflict --> toggle back & skip that path
+    else {
+      board.togglePiece(row, col);
+    }
+
+  }
+
+  traverse(0,0);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
-
+console.log(findNRooksSolution(12));
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
